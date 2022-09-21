78677116dcfcfea1512f1137c134d2b2b26a17a4
wxx <git@github.com:wxx70617/javatest.git>
2022-09-21 10:30:22 +0800

    设置字体，微调控件位置

--- recyclebin.cpp
+++ recyclebin.cpp
@@ -1,6 +1,7 @@
 #include "recyclebin.h"
 #include "ui_recyclebin.h"
 #include "setfontutil.h"
+#include "picturebox.h"
 RecycleBin::RecycleBin(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::RecycleBin)
@@ -10,6 +11,9 @@ RecycleBin::RecycleBin(QWidget *parent) :
     SetFontUtil::setMyFont(ui->lb_file);
     SetFontUtil::setMyFont(ui->lb_no_content);
     SetFontUtil::setMyFont(ui->lb_operation);
+    QImage *img = new QImage(":/images/no_content_in_recyclebin.png");
+    ui->recyclebin_pic->setMode(PictureBox::FIXED_SIZE);
+    ui->recyclebin_pic->setImage(*img);
 
 }
 
--- widget_bin.cpp
+++ widget_bin.cpp
@@ -1,6 +1,7 @@
 #include "widget_bin.h"
 #include "ui_widget_bin.h"
 #include "picturebox.h"
+#include "setfontutil.h"
 #include <QMouseEvent>
 Widget_Bin::Widget_Bin(QWidget *parent) :
     QWidget(parent),
@@ -9,7 +10,8 @@ Widget_Bin::Widget_Bin(QWidget *parent) :
     ui->setupUi(this);
     ui->widget_Bin->setStyleSheet("QWidget{background:#00FFFFFF;border-radius:15px}QWidget:hover{border:0px;background:rgb(215, 237, 255);}");
     ui->label_Bin->setAttribute(Qt::WA_TranslucentBackground);
-
+    //设置字体
+    SetFontUtil::setMyFont(ui->label_Bin);
     ui->binPic->setMode(PictureBox::FIXED_SIZE);
     QImage* imgBin = new QImage(":/images/bin.png");
     ui->binPic->setImage(*imgBin);
